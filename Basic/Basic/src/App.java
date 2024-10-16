6e0b9698a244bbeb839c5b2234cc4a0c26daa31a
CaptainCheese9 <daniel.koikkalainen@student.hamk.fi>
2024-10-16 18:35:48 +0300

    Tehtävä4
    Tehtävä4

--- Basic/Basic/src/App.java
+++ Basic/Basic/src/App.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-                String[] colors = {"Green", "Blue", "Yellow"};
+        String[] furniture = {"Table", "Sofa", "Shelf", "Painting"};
 
-                System.out.println("Value at index 2: " + colors);
-        
-                System.out.println("All values in the array:");
-                for (int i = 0; i < colors.length; i++) {
-                    System.out.println((i + 1) + ". " + colors[i]);
-                }
-        
+        // Loop through the array
+        for (int i = 0; i < furniture.length; i++) {
+            // Check if the value of the array item is "Sofa"
+            if (furniture[i].equals("Sofa")) {
+                System.out.println("Sofa found");
+            }
+        }
     }
 }
