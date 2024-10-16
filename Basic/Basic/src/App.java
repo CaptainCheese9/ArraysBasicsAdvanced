a84e41ca9dc8796d78e90c597d29ee011a2680e5
CaptainCheese9 <daniel.koikkalainen@student.hamk.fi>
2024-10-16 18:32:53 +0300

    Tehtävä3
    Tehtävä3basics

--- Basic/Basic/src/App.java
+++ Basic/Basic/src/App.java
@@ -2,31 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // String[] colors = {"Green", "Blue", "Yellow"};
-        // System.out.println("Blue");
-        // for (int i = 0; i < colors.length; i++) {
-        //     System.out.println(colors[i]);
-        // }
-        // Create an array and assign the aphorisms to it
-        String[] aphorisms = {
-            "Actions speak louder than words.",
-            "A barking dog never bites.",
-            "A penny saved is a penny earned.",
-            "All things come to those who wait."
-        };
 
-        // Ask the user to type a number from 1 to 4
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Pick number from 1-4.");
-        int choice = scanner.nextInt();
+                String[] colors = {"Green", "Blue", "Yellow"};
 
-        // Print the corresponding aphorism
-        if (choice >= 1 && choice <= 4) {
-            System.out.println(aphorisms[choice - 1]);
-        } else {
-            System.out.println("Invalid choice. Please pick a number from 1 to 4.");
-        }
-
-        scanner.close();
+                System.out.println("Value at index 2: " + colors);
+        
+                System.out.println("All values in the array:");
+                for (int i = 0; i < colors.length; i++) {
+                    System.out.println((i + 1) + ". " + colors[i]);
+                }
+        
     }
 }
